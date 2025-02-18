4f94360e67b9b5854c0186f1e4c66f0077cabaae
tobman <tl@braum.tobman.no>
2025-02-19 00:05:11 +0100

    debug

--- src/Shaco.API/Program.cs
+++ src/Shaco.API/Program.cs
@@ -1,5 +1,5 @@
 global using Shaco.API.Data.Models;
-
+using Microsoft.EntityFrameworkCore;
 using Shaco.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,5 +11,13 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.MapControllers();
+
+using(var scope = app.Services.CreateAsyncScope()) {
+  var db = scope.ServiceProvider.GetRequiredService<DB>();
+  var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+  logger.LogDebug("DB: {con}", db.Database.GetConnectionString());
+  await db.Database.MigrateAsync();
+}
 
 app.Run();
