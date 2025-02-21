9e90737c07bdd400de3fa7cd38b0a7191e24673c
tobman <tl@braum.tobman.no>
2025-02-21 13:50:25 +0100

    delete user command

--- cli/src/user.py
+++ cli/src/user.py
@@ -14,6 +14,15 @@ def setup_parser(parser: ArgumentParser):
   list_parser = action_parser.add_parser("list")
   list_parser.set_defaults(func=list)
 
+  rm_parser = action_parser.add_parser("rm")
+  rm_parser.set_defaults(func=delete)
+  
+  rm_parser.add_argument(
+    "-i", "--id",
+    help="ID of user to delete",
+    required=True
+  )
+
 def add_user(args: Namespace):
   print("Username: ", end="")
   username = input()
@@ -37,5 +46,10 @@ def add_user(args: Namespace):
 
 def list(args: Namespace):
   users = args.session.get("User")
+  
+  for user in users:
+    print(f"{user['id']}: {user['username']}")
 
-  print(users)
+def delete(args: Namespace):
+  args.session.delete(f"User/{args.id}")
+  print("Done")
