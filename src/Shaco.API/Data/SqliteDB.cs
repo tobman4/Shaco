1a3d8b35027c97e2e35a7283165a10fe6b882dea
tobman <tl@braum.tobman.no>
2025-02-21 10:28:44 +0100

    config db path

--- src/Shaco.API/Data/SqliteDB.cs
+++ src/Shaco.API/Data/SqliteDB.cs
@@ -6,11 +6,21 @@ public class SqliteDB : DB {
   
   private readonly string DbPath;
 
-  public SqliteDB() {
-    var folder = Environment.SpecialFolder.LocalApplicationData;
-    var path = Environment.GetFolderPath(folder);
-    DbPath = System.IO.Path.Join(path, "shaco.db");
+  public SqliteDB(
+    IConfiguration config
+  ) {
+    var path = config["DB"];
+    if(string.IsNullOrWhiteSpace(path))
+      throw new Exception("No db path");
+
+    DbPath = path;
   }
+
+  /* public SqliteDB() { */
+  /*   var folder = Environment.SpecialFolder.LocalApplicationData; */
+  /*   var path = Environment.GetFolderPath(folder); */
+  /*   DbPath = System.IO.Path.Join(path, "shaco.db"); */
+  /* } */
 
   protected override void OnConfiguring(DbContextOptionsBuilder options) =>
     options.UseSqlite($"Data Source={DbPath}");
